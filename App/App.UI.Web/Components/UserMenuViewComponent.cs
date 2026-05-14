dcabbaf18bff986d30282b5a40da85dcf9b58f07
fathur.rosi <fathur.rosi@ag-it.com>
2026-05-14 17:38:09 +0700

    setting layout

---
+++ App/App.UI.Web/Components/UserMenuViewComponent.cs
@@ -0,0 +1,21 @@
+﻿ 
+using Microsoft.AspNetCore.Mvc;
+
+namespace MANDe.Web.Components
+{
+
+    public class UserMenuViewComponent : ViewComponent
+    {
+        //private readonly ICurrentUserService _userService;
+        //public UserMenuViewComponent(ICurrentUserService userService)
+        //{
+        //    _userService = userService;
+        //}
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //UserDto item = await _userService.GetAsync();
+            //return View("~/Views/Shared/Layout/_SidebarUserMenu.cshtml", item);
+            return View("~/Views/Shared/Layout/_SidebarUserMenu.cshtml");
+        }
+    }
+}
