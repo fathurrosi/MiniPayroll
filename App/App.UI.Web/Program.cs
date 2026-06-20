8f533a1f6b33c403073aa2a5a6de6fb9bf73ddaf
fathur.rosi <fathur.rosi@ag-it.com>
2026-06-20 11:29:09 +0700

    pak angkasa nitip push

--- App/App.UI.Web/Program.cs
+++ App/App.UI.Web/Program.cs
@@ -5,8 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddServices(builder.Configuration, builder.Environment);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
-
+builder.Services.AddControllersWithViews(); 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Host.UseSerilog((context, services, configuration) =>
 {
     configuration
@@ -16,7 +17,6 @@ builder.Host.UseSerilog((context, services, configuration) =>
 });
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -24,6 +24,17 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    // AKTIFKAN SWAGGER UI DI MODE DEVELOPMENT
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        // Mengatur agar Swagger UI tahu di mana file json-nya berada
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+}
+
 
 app.UseHttpsRedirection();
 app.UseRouting();
