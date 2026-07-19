ca8e4eb97f38d2b03d6f096fd5a85d52c6661b18
fathur.rosi <fathur.rosi@ag-it.com>
2026-07-19 14:45:19 +0700

    add access permission

---
+++ App/App.Domain/Models/Dto/Settings/MenuPermissionDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.Domain.Models.Dto.Settings
+{
+    public class MenuPermissionDto
+    {
+        public long MenuId { get; set; }
+
+        public string MenuName { get; set; } = "";
+
+        public int Level { get; set; }
+
+        public bool HasChildren { get; set; }
+
+        public bool CanView { get; set; }
+
+        public bool CanCreate { get; set; }
+
+        public bool CanEdit { get; set; }
+
+        public bool CanDelete { get; set; }
+
+        public bool CanExport { get; set; }
+
+        public bool CanApprove { get; set; }
+    }
+}
